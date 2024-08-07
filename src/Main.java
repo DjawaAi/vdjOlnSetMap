784621cb4445920794d713241920b18c5c387a73
DjawaAi <dmitrypanyukov@gmail.com>
2024-08-07 23:03:59 +0300

    №1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        byte dZ = 0;
+        System.out.println();
+        System.out.println("Задача № " +(++dZ));
+        System.out.println();
+    }
+}
